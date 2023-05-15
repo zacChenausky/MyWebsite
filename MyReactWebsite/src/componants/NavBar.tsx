40adf30321ed00746f1af91e5b78677c0beb1162
Zachary Chenausky <zchenausky@gmail.com>
2023-05-15 18:06:22 -0500

    remove disable bar

--- MyReactWebsite/src/componants/NavBar.tsx
+++ MyReactWebsite/src/componants/NavBar.tsx
@@ -10,7 +10,6 @@ export default function Navbar() {
         <a className="nav-link active" aria-current="page" href="#">Home</a>
         <a className="nav-link" href="#">Features</a>
         <a className="nav-link" href="#">Pricing</a>
-        <a className="nav-link disabled">Disabled</a>
       </div>
     </div>
   </div>
